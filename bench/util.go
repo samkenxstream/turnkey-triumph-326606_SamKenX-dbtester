2e2f4a44b1ae56799cea29b8b2fcd6d79c7f4387
Gyu-Ho Lee <gyuhox@gmail.com>
2016-03-16 02:12:37 -0700

    bench: change bytes util

--- bench/util.go
+++ bench/util.go
@@ -21,6 +21,8 @@ import (
 	"os"
 	"time"
 
+	mrand "math/rand"
+
 	"github.com/coreos/etcd/clientv3"
 	"github.com/samuel/go-zookeeper/zk"
 )
@@ -80,11 +82,34 @@ func mustRandBytes(n int) []byte {
 	return rb
 }
 
+func randBytes(bytesN int) []byte {
+	const (
+		letterBytes   = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+		letterIdxBits = 6                    // 6 bits to represent a letter index
+		letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+		letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+	)
+	src := mrand.NewSource(time.Now().UnixNano())
+	b := make([]byte, bytesN)
+	for i, cache, remain := bytesN-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+	return b
+}
+
 func multiRandBytes(bytesN, sliceN int) [][]byte {
 	m := make(map[string]struct{})
 	var rs [][]byte
 	for len(rs) != sliceN {
-		b := mustRandBytes(bytesN)
+		b := randBytes(bytesN)
 		if _, ok := m[string(b)]; !ok {
 			rs = append(rs, b)
 			m[string(b)] = struct{}{}
