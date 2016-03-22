41fc849e5d4add1cef2b1a51004da8bf4f8dbfb8
Gyu-Ho Lee <gyuhox@gmail.com>
2016-03-22 00:01:58 -0700

    delete doc

--- bench/main.go
+++ bench/main.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// bench does benchmark.
+// bench does benchmark, mostly copied from https://github.com/coreos/etcd/tree/master/tools/benchmark.
 package main
 
 import (
