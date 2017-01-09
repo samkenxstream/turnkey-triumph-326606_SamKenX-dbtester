5e0d55e245a3b33c05d5dd845909fa6895499ab3
Gyu-Ho Lee <gyuhox@gmail.com>
2017-01-09 11:50:30 -0800

    agent: clean up 'Command' and flags

--- agent/util.go
+++ agent/util.go
@@ -19,19 +19,32 @@ import (
 	"runtime"
 )
 
-func openToAppend(fpath string) (*os.File, error) {
-	f, err := os.OpenFile(fpath, os.O_RDWR|os.O_APPEND, 0777)
+func openToRead(fpath string) (*os.File, error) {
+	f, err := os.OpenFile(fpath, os.O_RDONLY, 0444)
 	if err != nil {
-		f, err = os.Create(fpath)
-		if err != nil {
-			return f, err
-		}
+		return nil, err
+	}
+	return f, nil
+}
+
+func openToAppend(fpath string) (*os.File, error) {
+	f, err := os.OpenFile(fpath, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0777)
+	if err != nil {
+		return nil, err
+	}
+	return f, nil
+}
+
+func openToOverwrite(fpath string) (*os.File, error) {
+	f, err := os.OpenFile(fpath, os.O_RDWR|os.O_TRUNC|os.O_CREATE, 0777)
+	if err != nil {
+		return nil, err
 	}
 	return f, nil
 }
 
 func toFile(txt, fpath string) error {
-	f, err := os.OpenFile(fpath, os.O_RDWR|os.O_TRUNC, 0777)
+	f, err := openToOverwrite(fpath)
 	if err != nil {
 		f, err = os.Create(fpath)
 		if err != nil {
