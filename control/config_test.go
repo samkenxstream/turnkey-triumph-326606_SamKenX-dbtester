2b1927a38d43fa15d9f040d8807ff01562bc3d26
Gyu-Ho Lee <gyuhox@gmail.com>
2017-01-31 16:29:44 -0800

    control: fix test

--- control/config_test.go
+++ control/config_test.go
@@ -55,8 +55,8 @@ func TestReadConfig(t *testing.T) {
 	if c.Log != "control.log" {
 		t.Fatalf("unexpected %v", c.Log)
 	}
-	if c.DataSizeSummary != "data-size-summary.csv" {
-		t.Fatalf("unexpected %s", c.DataSizeSummary)
+	if c.DatasizeSummary != "data-size-summary.csv" {
+		t.Fatalf("unexpected %s", c.DatasizeSummary)
 	}
 	if c.DataLatencyDistributionSummary != "data-latency-distribution-summary.csv" {
 		t.Fatalf("unexpected %s", c.DataLatencyDistributionSummary)
