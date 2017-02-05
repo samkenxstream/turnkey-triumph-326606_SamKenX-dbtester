0f1fd9d369d392a42468cf39d8702790a99c5241
Gyu-Ho Lee <gyuhox@gmail.com>
2017-02-05 14:09:36 -0800

    Update 04-run-dbtester.sh

--- bench-configuration/04-run-dbtester.sh
+++ bench-configuration/04-run-dbtester.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-# agent; specify network interface, disk device on the machine
-# this starts the database on host machine when signalled from 'control'
+# agent; specify network interface, disk device of host machine,
+# this starts the database on host machine, when 'control' signals
 nohup dbtester agent --network-interface ens4  --disk-device sda1  --agent-port :3500 &
 
-# control; specify 'control' configuration file
-# this starts client stressing
+# control; specify 'control' configuration file (client number, key number, key-value size),
+# this starts database stressing, and shuts down the database when done
 nohup dbtester control -c config.yaml > $HOME/control.log 2>&1 &
 
 # analyze; get all data from remote machines
-# specify 'analyze' configuration file
-# this aggregates, generates all graphs, texts
+# and specify 'analyze' configuration file,
+# this aggregates data, generates all graphs, texts
 dbtester analyze --config analyze.yaml
