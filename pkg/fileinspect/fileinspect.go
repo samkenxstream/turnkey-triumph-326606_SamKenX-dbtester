3c83229cb8f6fbe4ba3f75b2f7694f35078dbde3
Gyu-Ho Lee <gyuhox@gmail.com>
2017-02-10 20:42:16 -0800

    Update fileinspect.go

--- pkg/fileinspect/fileinspect.go
+++ pkg/fileinspect/fileinspect.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package fileinspect implements various file inspecting utils.
 package fileinspect
 
 import (
