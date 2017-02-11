8af01cc3ded4c8f03eb59e0e971a18ce1056f951
Gyu-Ho Lee <gyuhox@gmail.com>
2017-02-10 20:41:30 -0800

    Update ntp.go

--- pkg/ntp/ntp.go
+++ pkg/ntp/ntp.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package ntp syncs system time with NTP server.
 package ntp
 
 import (
