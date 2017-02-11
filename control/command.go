c9deee70a066f3cd5254abcd20108bd40d59355c
Gyu-Ho Lee <gyuhox@gmail.com>
2017-02-10 20:43:41 -0800

    Update command.go

--- control/command.go
+++ control/command.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package control controls the database agents and benchmark testers.
 package control
 
 import (
