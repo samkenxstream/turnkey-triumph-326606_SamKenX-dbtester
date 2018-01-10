d4d2c3027cecc073c12f6bf6262f0b81810288b5
Gyuho Lee <gyuhox@gmail.com>
2018-01-09 19:31:21 -0800

    test-configs: update with v3.3
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

--- test-configs/install-cetcd.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-go get -v github.com/coreos/cetcd/cmd/cetcd
-cetcd -h
--- test-configs/install-zetcd.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-go get -v github.com/coreos/zetcd/cmd/zetcd
-zetcd -h
